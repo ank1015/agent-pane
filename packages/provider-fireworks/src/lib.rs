4540c4360c24d9dc861a1086a347fffce28f85e8
ananya <akblockchain15@gmail.com>
2026-08-22 12:03:42 +0530

    feat(fireworks): map portable chat requests

--- packages/provider-fireworks/src/lib.rs
+++ packages/provider-fireworks/src/lib.rs
@@ -3,9 +3,11 @@
 mod config;
 mod error;
 mod models;
+mod request;
 
 pub use config::{DEFAULT_FIREWORKS_TIMEOUT, FireworksConfig};
 pub use models::{FIREWORKS_MODELS, FireworksModel, calculate_usage_cost, find_model};
+pub use request::{FIREWORKS_NATIVE_INPUT_TAG, build_chat_completion_request};
 
 /// Provider identifier used by this package.
 pub const FIREWORKS_PROVIDER: &str = "fireworks";
